86f910f8b3ef8443679eac6610f9b78c9c75169c
Duane M Moody <dmmoody@gmail.com>
2015-01-30 14:01:06 -0800

    add venue pages

--- app.rb
+++ app.rb
@@ -40,3 +40,19 @@ post('/venues') do
   @bands = Band.all()
   redirect('/')
 end
+
+get('/venues/:id') do
+  @venue = Venue.find(params.fetch("id").to_i())
+  @bands = Band.all()
+  erb(:venue)
+end
+
+post('/venues/:id') do
+  band = params.fetch("band").to_i()
+  @band = Band.find(band)
+  venue_id = params.fetch("id").to_i()
+  @venue = Venue.find(venue_id)
+  @venue.bands.push(@band)
+  @bands = Band.all()
+  erb(:venue)
+end
