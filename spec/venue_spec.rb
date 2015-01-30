a7c629c9b252b00f4a49a7ba30b5c8c56105e3bf
Duane M Moody <dmmoody@gmail.com>
2015-01-30 11:27:18 -0800

    add name validation for venue

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,7 +2,7 @@ require('spec_helper')
 
 describe(Venue) do
   it('validates presence of name') do
-    venue = Venue.new(:name => false)
+    venue = Venue.new(:name => "")
     expect(venue.save()).to(eq(false))
   end
 end
