395bac284ebd28a7eceb4511bec15782024534b0
Duane M Moody <dmmoody@gmail.com>
2015-01-30 10:50:36 -0800

    create venues table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130180936) do
+ActiveRecord::Schema.define(version: 20150130184906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bands", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "venues", force: :cascade do |t|
     t.string "name"
   end
 
