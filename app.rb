8d6737f84bb8113103ba21861124fa805142f47f
Duane M Moody <dmmoody@gmail.com>
2015-01-30 10:36:28 -0800

    add band list and new band form

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require('bundler/setup')
 Bundler.require(:default)
 
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| do file }
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @bands = Band.all()
   erb(:index)
 end
