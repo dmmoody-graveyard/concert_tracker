dbe0eda6e263b26c38bef576b0e5bad5c0544039
Duane M Moody <dmmoody@gmail.com>
2015-01-30 10:19:00 -0800

    add band class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,11 @@ require('bundler/setup')
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Band.all().each() do |band|
+      band.destroy()
+    end
+  end
+end
