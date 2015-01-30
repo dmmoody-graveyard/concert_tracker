2062d542c261ca06a0d31720c6c9e864ce26ef9e
Duane M Moody <dmmoody@gmail.com>
2015-01-30 12:48:14 -0800

    add have_and_belong_to_many spec

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,6 +1,9 @@
 require('spec_helper')
 
 describe(Venue) do
+
+  it { should have_and_belong_to_many(:bands) }
+
   it('validates presence of name') do
     venue = Venue.new(:name => "")
     expect(venue.save()).to(eq(false))
