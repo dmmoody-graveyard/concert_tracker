fd6f00242cb2702dd2abb094e28ac631425674d3
Duane M Moody <dmmoody@gmail.com>
2015-01-30 12:16:14 -0800

    add #title_case and spec

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,12 @@
 class Venue < ActiveRecord::Base
   validates(:name, :presence => true)
+  before_save(:title_case)
+
+private
+
+  define_method(:title_case) do
+    if self.name != self.name.upcase
+      self.name = (name().split(/(\W)/).map(&:capitalize).join)
+    end
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -5,4 +5,15 @@ describe(Venue) do
     venue = Venue.new(:name => "")
     expect(venue.save()).to(eq(false))
   end
+
+  it('takes a name and makes it titlecase') do
+    venue = Venue.create({:name => 'dolly parton', :id => nil})
+    expect(venue.name()).to(eq('Dolly Parton'))
+  end
+
+  it('takes a name and if upcase returns name') do
+    venue = Venue.create({:name => 'ACDC', :id => nil})
+    expect(venue.name()).to(eq('ACDC'))
+  end
+
 end
